3dba62aaf2f02c882a7aca7a501a4af7837f4beb
hscottvo <hscottvo@gmail.com>
2021-01-02 23:20:50 -0800

    initial commit

---
+++ practice_1.py
@@ -0,0 +1,100 @@
+# ----------------------------------------- IMPORTANT INFORTMATION -------------------------------------------------------
+# In these practice problems, questions will be commented out by default. When you work on a problem, uncomment the relevant code
+# by selecting the lines and pressing CTRL + / (Windows) or CMD + / (Mac). 
+# For example, for problem 0, we select from PROBLEM 0 START to PROBLEM 0 END
+# Try it below
+
+
+# # ---------- PROBLEM 0 START ----------
+# print('hello world!')
+# # ---------- PROBLEM 0 END ----------
+
+
+# When you are finished with the problem, run it to make sure the output matches what you expect. Then, re-comment that problem 
+# by using the same method and move on to the next problem! 
+
+
+# ---------------------------------------- PROBLEM 1 START ----------------------------------------
+# You are at the Apple store buying apples (what?).
+# You want to know how much money you will spend if you buy a certain amount of apples. 
+# Write code that takes in user input for the number of apples, converts it into an integer, 
+# and calculates the total cost. 
+# You MUST use variables to store the number of apples AND total cost.
+
+# BONUS: format the print statement so that it prints out: 
+# ___ apples costs $______.
+# hint: You can't print integers and strings together. What CAN you print together? 
+
+cost_apples = .4
+
+
+# ----------------- SOLUTION GOES HERE --------------------
+
+
+# ---------------------------------------------------------
+
+print(total_cost)
+
+# ----------------------------------------PROBLEM 1 END ----------------------------------------
+
+# ---------------------------------------- PROBLEM 2 START ----------------------------------------
+
+
+grocery_list = ['apples', 'bananas', 'pineapples', 'flour', 'chives', 'milk', 'garlic']
+
+# ---------- CODE GOES BELOW ---------- 
+
+# 1. Print 'flour' by indexing the grocery list.
+
+
+# 2. Print 'milk'
+
+
+# 3. Print the first 5 items of the grocery list in 1 line. 
+
+
+# 4. Add "cheeese" to the list.
+
+
+# 5. Remove "pineapples" from the list.
+
+
+# 6. Print the length of the list.
+
+
+# 7. Replace "bananas" with "cookies" in the list.
+
+
+# 8. Print the whole list. 
+
+
+# ---------------------------------------- PROBLEM 2 END ----------------------------------------
+
+# ---------------------------------------- PROBLEM 3 START ----------------------------------------
+# This is a challenge problem! In programming, swapping values is an important skill to have. 
+# 
+
+# Swap the values in variables a and b.
+a = input("Type in a value for variable a: ")
+b = input("Type in a value for variable b: ")
+
+# ---------- SOLUTION GOES HERE ----------
+
+
+# --------------------------------------- 
+
+print('a is now: ', a)
+print('b is now: ', b)
+
+# ---------------------------------------- PROBLEM 3 END ----------------------------------------
+
+# ---------------------------------------- PROBLEM 4 START ----------------------------------------
+
+# Write code that will use the input() method to take information as prompted. Then combine them into a sentence using the template below and print. 
+# TEMPLATE: 
+# "Hello, my name is _____, and I am _____ year(s) old. I am a ______ major."
+# Replace the blanks for name, age, and major respectively. 
+
+# ---------- CODE GOES BELOW ----------
+
+# ---------------------------------------- PROBLEM 4 END ----------------------------------------
---
+++ practice_2.py
@@ -0,0 +1,157 @@
+# ----------------------------------------- IMPORTANT INFORTMATION -------------------------------------------------------
+# In these practice problems, questions will be commented out by default. When you work on a problem, uncomment the relevant code
+# by selecting the lines and pressing CTRL + / (Windows) or CMD + / (Mac). 
+# For example, for problem 0, we select from PROBLEM 0 START to PROBLEM 0 END
+# Try it below
+
+
+# # ---------- PROBLEM 0 START ----------
+# print('hello world!')
+# # ---------- PROBLEM 0 END ----------
+
+
+# When you are finished with the problem, run it to make sure the output matches what you expect. Then, re-comment that problem 
+# by using the same method and move on to the next problem! 
+
+
+
+# # ---------------------------------------- PROBLEM 1 START ----------------------------------------
+# # Set x to a value such that the output is false
+
+# # ---------- SOLUTION GOES HERE ----------
+# x = 
+# # ----------------------------------------
+
+# print(x > 5)
+
+# # Set y to a value such that the output is true
+
+# # ---------- SOLUTION GOES HERE ----------
+# y = 
+# # ----------------------------------------
+
+# print(y or False)
+
+# # Set z to a value such that the output is true. 
+
+# # ---------- SOLUTION GOES HERE ----------
+# x = 
+# y = 
+# z = 
+# # ----------------------------------------
+
+# print(z and (not x or not y))
+
+# # ---------------------------------------- PROBLEM 1 END ----------------------------------------
+
+
+
+# # ---------------------------------------- PROBLEM 2 START ----------------------------------------
+
+# # Write code that takes age as an input, and outputs legal drinking eligibility (drinking age in the United States is 21 years).
+# # Example input/output: 
+# # 5
+# # You are not old enough to drink!
+
+# user_input = input("Welcome to Scotty's bar. How old are you? \n")
+
+# # ---------- SOLUTION GOES HERE ----------
+
+# # ----------------------------------------
+
+
+# # hint: You'll have to typecast the input!
+# # ---------------------------------------- PROBLEM 2 END ----------------------------------------
+
+
+
+# # ---------------------------------------- PROBLEM 3 START ----------------------------------------
+
+# # Using a for loop, print out each item in the given shopping list.
+
+# mall_trip = ['iPhone', 'Yeezys', 'perfume', 'laptop', 'Nintendo Switch', 'jacket']
+
+# # ---------- SOLUTION GOES HERE ----------
+
+# # ----------------------------------------
+
+# # Using a for loop, print out the first 3 items in mall_trip. Remember the range() method!
+
+# # ---------- SOLUTION GOES HERE ----------
+
+# # ----------------------------------------
+
+# # ---------------------------------------- PROBLEM 3 END ----------------------------------------
+
+
+
+# # ---------------------------------------- PROBLEM 4 START - CHALLENGE ----------------------------------------
+# # We are going to be making a game, where the computer randomly chooses a number (that will be given) between 1 and 100.
+# # You will then need to input a guess and print into the terminal whether the guess is correct, too high, or too low.
+# # When the guess is correct, the game will end. 
+
+# import random
+# target = random.randint(1,100)
+
+# # ---------- SOLUTION GOES HERE ----------
+# while True:
+#     guess = int(input("Guess a number between 1 and 100: "))
+
+# # ----------------------------------------
+
+# # Hint 1: Remember to use a break statement when needed
+# # Hint 2: This will require a lot of conditional statements!
+# # ---------------------------------------- PROBLEM 4 END ----------------------------------------
+
+# ---------------------------------------- PROBLEM 5 START ----------------------------------------
+# Write a function called add_five() that takes a number as an input and returns that number + 5. 
+
+x = int(input("Type in a number: "))
+
+# ---------- SOLUTION GOES HERE ----------
+
+# ----------------------------------------
+
+print(str(x) + " + 5 is " + str(add_five(x)))
+# ---------------------------------------- PROBLEM 5 END ----------------------------------------
+
+
+
+# ---------------------------------------- PROBLEM 6 START - CHALLENGE ----------------------------------------
+# The Fibonacci sequence is a sequence of numbers, where each number is the sum of the previous 2 numbers.
+# For example, the first few numbers in the sequence are: 
+# 1 1 2 3 5 8 13 21 34
+# Write a function that takes in a number, and prints that many terms of the series. 
+# For example: 
+# print_fib(5) prints: 
+# 1
+# 1
+# 2
+# 3
+# 5
+
+# 
+
+# ---------- SOLUTION GOES HERE ----------
+def print_fib(n): 
+    n1 = 1
+    n2 = 1
+    current_term = 0
+    temp = 0
+    if n <= 0:
+        print("Enter a Positive Number")
+    else:
+        print("Fibonacci sequence up to", n, ":")
+    while current_term < n:
+        print("Current term: "+  str(current_term+1) + ": " + str(n1))
+        
+
+# ----------------------------------------
+
+n = int(input("How many terms? "))
+
+# ---------- SOLUTION GOES HERE ----------
+
+# ----------------------------------------
+
+# ---------------------------------------- PROBLEM 6 END ----------------------------------------
